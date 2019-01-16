c0f8f814fc23538169a1babf4bd92d09d92a60d5
Paul Ross <apaulross@gmail.com>
2019-01-16 12:49:58 +0000

    Minor reorder of benchmarks.

--- benchmarks/TotalDepth/LIS/core/BenchmarkRepCode.py
+++ benchmarks/TotalDepth/LIS/core/BenchmarkRepCode.py
@@ -18,17 +18,17 @@ class TimeRepCode68:
     BYTES_153 = b'\x44\x4C\x80\x00''
     BYTES_0 = b'\x40\x00\x00\x00''
 
-    def time_cRepCode_to(self):
-        cRepCode.to68(153.0)
-
-    def time_cRepCode_from(self):
-        cRepCode.from68(0x444C8000)
-
     def time_pRepCode_to(self):
         pRepCode.to68(153.0)
 
     def time_pRepCode_from(self):
         pRepCode.from68(0x444C8000)
+
+    def time_cRepCode_to(self):
+        cRepCode.to68(153.0)
+
+    def time_cRepCode_from(self):
+        cRepCode.from68(0x444C8000)
 
     def time_cpRepCode_to(self):
         cpRepCode.to68(153.0)
