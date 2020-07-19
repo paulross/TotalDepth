5836fc13fdc4fb9a9e23a7eed4926742a7b99fad
Paul Ross <apaulross@gmail.com>
2020-07-19 11:40:27 +0100

    Add EBCDIC and SEG-Y support.

---
+++ src/TotalDepth/util/SEGY.py
@@ -0,0 +1,54 @@
+"""
+SEG-Y Format support.
+
+Defined in 'digital tape standards' by the Society of Exploration Geophysicists. Library of Congress Catalog Card Number
+80-52678. ISBN 0-9311830-15-X.
+Dated 1967 to 1992.
+
+Other sources:
+http://www.seg.org/documents/10161/77915/seg_y_rev0.pdf
+http://www.seg.org/documents/10161/77915/seg_a_b_ex.pdf
+http://seg.org/Portals/0/SEG/News%20and%20Resources/Technical%20Standards/seg_y_rev2_0-mar2017.pdf
+
+SEG General:
+https://seg.org/Publications/SEG-Technical-Standards
+
+Informally: https://en.wikipedia.org/wiki/SEG-Y
+
+
+All TotalDepth Supports at the moment is recognising a SEG-Y file.
+"""
+import typing
+
+from TotalDepth.util import EBCDIC
+
+
+CARD_IMAGE_EBCDIC_NUM_CARDS = 40
+CARD_WIDTH_EBCDIC = 80
+CARD_IMAGE_EBCDIC_BLOCK_LENGTH = CARD_IMAGE_EBCDIC_NUM_CARDS * CARD_WIDTH_EBCDIC
+
+
+def is_segy(file: typing.BinaryIO) -> bool:
+    """
+    Returns True if the file is likely to be a SEG-Y file.
+    This reads the first 3200 bytes of the file (if available).
+    These must all be EBCDIC printable characters.
+    Each 'card' of 80 characters must start with 'Cnn' where nn is from '01' to '40'.
+    """
+    file.seek(0)
+    byt = file.read(CARD_IMAGE_EBCDIC_BLOCK_LENGTH)
+    ret = False
+    if len(byt) == CARD_IMAGE_EBCDIC_BLOCK_LENGTH:
+        if EBCDIC.ebcdic_all_printable(byt):
+            as_ascii = EBCDIC.ebcdic_to_ascii(byt)
+            cards = [
+                as_ascii[i * CARD_WIDTH_EBCDIC:(i + 1) * CARD_WIDTH_EBCDIC] for i in range(CARD_IMAGE_EBCDIC_NUM_CARDS)
+            ]
+            for i, card in enumerate(cards):
+                if card[0] != 'C':
+                    break
+                if int(card[1:3]) != i + 1:
+                    break
+            else:
+                ret = True
+    return ret
