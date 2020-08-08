ba9a8a04f6a5d7dbea656f0e83844820f6ac4073
Paul Ross <apaulross@gmail.com>
2020-08-08 11:12:09 +0100

    WIP: PEP-8 DumpFrameSet.

--- src/TotalDepth/LIS/DumpFrameSet.py
+++ src/TotalDepth/LIS/DumpFrameSet.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Part of TotalDepth: Petrophysical data processing and presentation
 # Copyright (C) 1999-2011 Paul Ross
 # 
@@ -20,73 +20,71 @@
 """Reads a LIS file and writes out tab separated values of each frame.
 
 Created on 25 Mar 2011
-
-@author: p2ross
 """
-__author__  = 'Paul Ross'
-__date__    = '2010-08-02'
-__version__ = '0.1.0'
-__rights__  = 'Copyright (c) Paul Ross'
-
 import time
 import sys
 import os
 import logging
 from optparse import OptionParser
 
-#from TotalDepth.LIS import ExceptionTotalDepthLIS
 from TotalDepth.LIS.core import File
 from TotalDepth.LIS.core import FileIndexer
 from TotalDepth.LIS.core import FrameSet
 
-def dumpFrameSets(fp, keepGoing, summaryOnly, channels):
+
+__author__  = 'Paul Ross'
+__date__    = '2010-08-02'
+__version__ = '0.1.0'
+__rights__  = 'Copyright (c) Paul Ross'
+
+
+def dump_frame_sets(fp, keep_going, summary_only, channels):
     """Dump the frame values to stdout.
 
-    keepGoing is a bool.
-    SummaryOnly is a bool to emit a summary only, if false all the data and the summary is written out.
-    Channels is a set of Mnems, if non-empty then only these channels, if present, are written out."""
+    summary_only is a bool to emit a summary only, if false all the data and the summary is written out.
+    channels is a set of Mnems, if non-empty then only these channels, if present, are written out."""
     logging.info('Index.indexFile(): {:s}'.format(fp))
     assert(os.path.isfile(fp))
-    myFi = File.FileRead(fp, theFileId=fp, keepGoing=keepGoing)
-    myIdx = FileIndexer.FileIndex(myFi)
-    for aLp in myIdx.genLogPasses():
+    lis_file = File.FileRead(fp, theFileId=fp, keepGoing=keep_going)
+    lis_index = FileIndexer.FileIndex(lis_file)
+    for aLp in lis_index.genLogPasses():
         print(aLp)
         # Load the FrameSet
         if aLp.logPass.totalFrames == 0:
             print('No frames to load.')
         else:
-            aLp.logPass.setFrameSet(myFi, None, None)
-            myFrSet = aLp.logPass.frameSet
-            if not summaryOnly:
+            aLp.logPass.setFrameSet(lis_file, None, None)
+            frame_set = aLp.logPass.frameSet
+            if not summary_only:
                 # Print the channels and units
-                hdrS = []
-                if myFrSet.isIndirectX:
-                    hdrS.append('XAXIS [{!r:s}]'.format(myFrSet.xAxisDecl.depthUnits))
+                headers = []
+                if frame_set.isIndirectX:
+                    headers.append('XAXIS [{!r:s}]'.format(frame_set.xAxisDecl.depthUnits))
                 indexes = []
                 if len(channels):
                     for i, (m, u) in enumerate(aLp.logPass.genFrameSetHeadings()):
                         if m in channels:
-                            hdrS.append('{!r:s} [{!r:s}]'.format(m, u))
+                            headers.append('{!r:s} [{!r:s}]'.format(m, u))
                             indexes.append(i)
                 else:
-                    hdrS.extend(['{!r:s} [{!r:s}]'.format(m, u) for m,u in aLp.logPass.genFrameSetHeadings()])
+                    headers.extend(['{!r:s} [{!r:s}]'.format(m, u) for m, u in aLp.logPass.genFrameSetHeadings()])
                 if len(indexes) == len(channels):
                     logging.warning(
-                        'Some channels you specified can not be found: indexes={!r:s} channels={!r:s}'.format(indexes, channels)
+                        'Some channels you specified can not be found: indexes={!r:s} channels={!r:s}'.format(
+                            indexes, channels
+                        )
                     )
-                #print('TRACE: len(hdrS)', len(hdrS))
-                print('\t'.join(hdrS))
-                for frIdx in range(myFrSet.numFrames):
-                    #print('TRACE: len(frame)', len(myFrSet.frame(frIdx)))
-                    if myFrSet.isIndirectX:
-                        print(myFrSet.xAxisValue(frIdx), '\t', end='')
+                print('\t'.join(headers))
+                for frIdx in range(frame_set.numFrames):
+                    if frame_set.isIndirectX:
+                        print(frame_set.xAxisValue(frIdx), '\t', end='')
                     if len(indexes):
-                        values = [myFrSet.frame(frIdx)[i] for i in indexes]
+                        values = [frame_set.frame(frIdx)[i] for i in indexes]
                         print('\t'.join(['%g' % v for v in values]))
                     else:
-                        print('\t'.join(['%g' % v for v in myFrSet.frame(frIdx)]))
+                        print('\t'.join(['%g' % v for v in frame_set.frame(frIdx)]))
             # Accumulate min/mean/max
-            myAccClasses = [
+            accumulate_classes = [
                     FrameSet.AccCount,
                     FrameSet.AccMin,
                     FrameSet.AccMean,
@@ -99,56 +97,55 @@ def dumpFrameSets(fp, keepGoing, summaryOnly, channels):
                     FrameSet.AccDrift,
                     FrameSet.AccActivity,
             ]
-            myAcc = myFrSet.accumulate(myAccClasses)
+            accumulated_data = frame_set.accumulate(accumulate_classes)
             print()
-            fmtStr = '{:12s} ' + (' {:>12s}'*len(myAccClasses)) 
-            print(fmtStr.format(
+            format_str = '{:12s} ' + (' {:>12s}'*len(accumulate_classes))
+            print(format_str.format(
                     'Sc Name', 'Count', 'Min', 'Mean', 'Max', 'Std Dev.', '--', '==', '++', 'Bias', 'Drift', 'Activity',
                 )
             )
-            schNameS = list(aLp.logPass.genFrameSetScNameUnit())
-#            print(schNameS)
-            for scIdx, aRow in enumerate(myAcc):
-                print('{:4s} [{:4s}]'.format(*schNameS[scIdx]),
+            sub_channel_names_and_units = list(aLp.logPass.genFrameSetScNameUnit())
+            for scIdx, aRow in enumerate(accumulated_data):
+                print('{:4s} [{:4s}]'.format(*sub_channel_names_and_units[scIdx]),
                       ' ',
                       ' '.join(['{:12.5g}'.format(v) for v in aRow]))
+
 
 def main():
     usage = """usage: %prog [options] dir
 Reads a LIS file and writes out tab separated values of each frame."""
-    print ('Cmd: %s' % ' '.join(sys.argv))
-    optParser = OptionParser(usage, version='%prog ' + __version__)
-    optParser.add_option("-k", "--keep-going", action="store_true", dest="keepGoing", default=False, 
-                      help="Keep going as far as sensible. [default: %default]")
-    optParser.add_option(
+    print('Cmd: %s' % ' '.join(sys.argv))
+    option_parser = OptionParser(usage, version='%prog ' + __version__)
+    option_parser.add_option("-k", "--keep-going", action="store_true", dest="keepGoing", default=False,
+                             help="Keep going as far as sensible. [default: %default]")
+    option_parser.add_option(
             "-l", "--loglevel",
             type="int",
             dest="loglevel",
             default=20,
             help="Log Level (debug=10, info=20, warning=30, error=40, critical=50) [default: %default]"
         )      
-    optParser.add_option("-s", "--summary", action="store_true", dest="summary", default=False, 
-                      help="Display summary only. [default: %default]")
-    optParser.add_option("-c", "--channels", action="append", type="str",
-                         help="Only dump these named curves.", default=[])
-    opts, args = optParser.parse_args()
-    clkStart = time.perf_counter()
+    option_parser.add_option("-s", "--summary", action="store_true", dest="summary", default=False,
+                             help="Display summary only. [default: %default]")
+    option_parser.add_option("-c", "--channels", action="append", type="str",
+                             help="Only dump these named curves.", default=[])
+    opts, args = option_parser.parse_args()
+    clock_start = time.perf_counter()
     # Initialise logging etc.
     logging.basicConfig(level=opts.loglevel,
-                    format='%(asctime)s %(levelname)-8s %(message)s',
-                    #datefmt='%y-%m-%d % %H:%M:%S',
-                    stream=sys.stdout)
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        stream=sys.stdout)
     # Your code here
     if len(args) != 1:
-        optParser.print_help()
-        optParser.error("I can't do much without a path to the LIS file.")
+        option_parser.print_help()
+        option_parser.error("I can't do much without a path to the LIS file.")
         return 1
-    dumpFrameSets(args[0], opts.keepGoing, opts.summary, set([v.encode('ascii') for v in opts.channels]))
-    clkExec = time.perf_counter() - clkStart
-    print('CPU time = %8.3f (S)' % clkExec)
+    dump_frame_sets(args[0], opts.keepGoing, opts.summary, set([v.encode('ascii') for v in opts.channels]))
+    clock_exec = time.perf_counter() - clock_start
+    print('CPU time = %8.3f (S)' % clock_exec)
     print('Bye, bye!')
     return 0
 
+
 if __name__ == '__main__':
-    #multiprocessing.freeze_support()
     sys.exit(main())
